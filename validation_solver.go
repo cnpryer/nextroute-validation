bc60b520582cfb618c93e2a5b4ef8b2269e4c1ad
Chris Pryer <cnpryer@gmail.com>
2025-07-31 21:08:41 -0400

    Remove old comment

--- validation_solver.go
+++ validation_solver.go
@@ -10,16 +10,6 @@ func NewValidationParallelSolver(model nextroute.Model) (nextroute.ParallelSolve
 		return nil, err
 	}
 
-	// Set the solver factory for the parallel solver. This factory is used to
-	// create new solver instances for each cycle. The information contains data
-	// about the current cycle and which solver of the n solvers is being
-	// created. The solution is the best solution of the previous cycle (and
-	// globally best).
-	//
-	// In this example we create identical solvers with custom operators, but
-	// you can also create different solvers with different operators. There
-	// is a random component in the operators, so the solvers will behave
-	// differently.
 	parallelSolver.SetSolverFactory(
 		func(
 			information nextroute.ParallelSolveInformation,
